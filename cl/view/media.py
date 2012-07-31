3106ead4041240639182bf2ffa9bdd7e24be99fc
Mark MacGillivray <mark@cottagelabs.com>
2012-07-31 14:01:37 +0100

    adding fix for media directory

--- cl/view/media.py
+++ cl/view/media.py
@@ -13,20 +13,21 @@ import cl.util as util
 blueprint = Blueprint('media', __name__)
 
 
+mediadir = os.path.dirname(os.path.abspath(__file__)).replace('/cl/view','/') + app.config['MEDIA_FOLDER']
+
 @blueprint.route('/')
 def media():
     jsite = deepcopy(app.config['JSITE_OPTIONS'])
     jsite['data'] = False
     jsite['editable'] = False
     jsite['facetview']['initialsearch'] = False
-    listing = os.listdir( app.config['MEDIA_FOLDER'] )
+    listing = os.listdir( mediadir )
     if util.request_wants_json():
         response = make_response( json.dumps(listing,"","    ") )
         response.headers["Content-type"] = "application/json"
         return response
     else:
-        # TODO: this may be obsoleted - or else add some sort of delete and other 
-        # functionality to the media page display...
+        # TODO: add some sort of delete and other functionality to the media page display...
         files = ''
         for f in listing:
             if f.split('.')[-1] in ['png','jpg','jpeg','gif']:
@@ -40,9 +41,9 @@ def media():
 
 @blueprint.route('/<path:path>', methods=['GET','POST'])
 def medias(path=''):
-    # TODO: this GET is just for dev. This should be a setting on the nginx server
+    # TODO: this should be a setting on the nginx server
     if request.method == 'GET':
-        loc = app.config['MEDIA_FOLDER'] + '/' + path
+        loc = mediadir + '/' + path
         if app.config['DEBUG'] and os.path.isfile(loc):
             response = make_response(open(loc).read())
             response.headers["Content-type"] = "image"
@@ -54,7 +55,7 @@ def medias(path=''):
         if not current_user.is_anonymous():
             # TODO: check the file type meets the allowed ones, and if so put it in media dir
             filename = werkzeug.secure_filename(path)
-            out = open(app.config['MEDIA_FOLDER'] + '/' + filename, 'w')
+            out = open(mediadir + '/' + filename, 'w')
             out.write(request.data)
             out.close()
             return ''
